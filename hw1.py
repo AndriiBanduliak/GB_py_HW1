583c2420c4304a9f711f8e12f5c2d447916986ee
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 15:13:40 +0300

    5th task is done by function

--- hw1.py
+++ hw1.py
@@ -1,20 +1,31 @@
-'''4. Напишите программу, которая по заданному номеру четверти, показывает 
-диапазон возможных координат точек в этой четверти (x и y). - x=34; y=-30 -> 4
-- x=2; y=4-> 1
-- x=-34; y=-30 -> 3'''
+""" Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве. 
+Пример: - A (3,6); B (2,1) -> 5,09 - A (7,-5); B (1,-1) -> 7,21 """
 
 
+def intNum(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Enter the coordinate for {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("You wrong. Enter whole numbers!")
+    return a
 
-X=int(input('Enter the x '))
-Y=int(input('Enter the y '))
-    
-if X > 0 and  Y > 0:
-    print("The point in the 1th quarter ")
-elif X < 0 and Y > 0:
-    print('The point in the 2nd quarter')
-elif X <0 and Y < 0:
-    print('The point in the 3rd quarter')
-elif X > 0 and Y < 0:
-    print('The point in the 4th quarter')
-else:
-    print("It's the center of the coordinates")
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Enter point the 'A' coordinates") 
+pointA = intNum(2)
+print("Enter point the 'B' coordinates")
+pointB = intNum(2)
+
+print(f"Cut length: {format(calculateLengthSegment(pointA, pointB), '.3f')}")
