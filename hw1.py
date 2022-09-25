b5260b287d72bf2d2ca51320d0d22dfa4c402d98
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 15:10:26 +0300

    3rd task is done by function

--- hw1.py
+++ hw1.py
@@ -5,20 +5,33 @@
 - x=2; y=4-> 1
 - x=-34; y=-30 -> 3'''
 
-while True:
-    print('X ≠ 0 и Y ≠ 0 ')
-    x=int(input('Enter the number representing the coordinate X: '))
-    y=int(input('Enter the number representing the coordinate Y: '))
-    if not(y==0 or x==0):
-        break
-print('x=',x, 'y=',y, '-> ', end='')
-if (x>0):
-    if (y>0):
-        print('1')
-    else:
-        print('4')
-if (x<0):
-    if (y>0):
-        print('2')
-    else:
-        print('3')
+def inputKoord(x):
+    a = [0] * x
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("Координата не должно быть равна 0 ")
+            except ValueError:
+                print("Ты ошибся. Вводить надо числа!")
+    return a
+
+
+def checkCoordinates(xy):
+    text = 4
+    if xy[0] > 0 and xy[1] > 0:
+        text = 1
+    elif xy[0] < 0 and xy[1] > 0:
+        text = 2
+    elif xy[0] < 0 and xy[1] < 0:
+        text = 3
+    print(f"Точка находится в {text} четверти плоскости")
+
+3
+koordinate = inputKoord(2)
+checkCoordinates(koordinate)
