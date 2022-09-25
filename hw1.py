506e7a646aa329a1672b165ed4d6a5f08e0a4338
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 16:24:13 +0300

    5th task is done

--- hw1.py
+++ hw1.py
@@ -2,30 +2,15 @@
 и находит расстояние между ними в 2D пространстве. 
 Пример: - A (3,6); B (2,1) -> 5,09 - A (7,-5); B (1,-1) -> 7,21 """
 
+print("Задайте координаты точки А")
+x1 = int(input("1: "))
+x2 = int(input("2: "))
+print(x1, x2)
 
-def intNum(x):
-    xy = ["X", "Y"]
-    a = []
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = int(input(f"Enter the coordinate for {xy[i]}: "))
-                a.append(number)
-                is_OK = True
-            except ValueError:
-                print("You wrong. Enter whole numbers!")
-    return a
+print("Задайте координаты точки B")
+y1 = int(input("1: "))
+y2 = int(input("2: "))
+print(y1, y2)
 
-
-def calculateLengthSegment(a, b):
-    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
-    return lengthSegment
-
-
-print("Enter point the 'A' coordinates") 
-pointA = intNum(2)
-print("Enter point the 'B' coordinates")
-pointB = intNum(2)
-
-print(f"Cut length: {format(calculateLengthSegment(pointA, pointB), '.3f')}")
+result = pow( ( ( (y1 - x1) * (y1 - x1) ) + ( (y2 - x2) * (y2 - x2) ) ), 0.5)
+print(round(result,3))
