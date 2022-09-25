6c8fff8d43f4dac572c00b26298949db25cccab9
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 15:12:13 +0300

    4th task is done

--- hw1.py
+++ hw1.py
@@ -1,37 +1,20 @@
-'''3. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
- выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
-*Пример:*
-- x=34; y=-30 -> 4
+'''4. Напишите программу, которая по заданному номеру четверти, показывает 
+диапазон возможных координат точек в этой четверти (x и y). - x=34; y=-30 -> 4
 - x=2; y=4-> 1
 - x=-34; y=-30 -> 3'''
 
-def inputKoord(x):
-    a = [0] * x
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = float(input(f"Введите {i+1} координату: "))
-                a[i] = number
-                is_OK = True
-                if a[i] == 0:
-                    is_OK = False
-                    print("Координата не должно быть равна 0 ")
-            except ValueError:
-                print("Ты ошибся. Вводить надо числа!")
-    return a
 
 
-def checkCoordinates(xy):
-    text = 4
-    if xy[0] > 0 and xy[1] > 0:
-        text = 1
-    elif xy[0] < 0 and xy[1] > 0:
-        text = 2
-    elif xy[0] < 0 and xy[1] < 0:
-        text = 3
-    print(f"Точка находится в {text} четверти плоскости")
-
-3
-koordinate = inputKoord(2)
-checkCoordinates(koordinate)
+X=int(input('Enter the x '))
+Y=int(input('Enter the y '))
+    
+if X > 0 and  Y > 0:
+    print("The point in the 1th quarter ")
+elif X < 0 and Y > 0:
+    print('The point in the 2nd quarter')
+elif X <0 and Y < 0:
+    print('The point in the 3rd quarter')
+elif X > 0 and Y < 0:
+    print('The point in the 4th quarter')
+else:
+    print("It's the center of the coordinates")
