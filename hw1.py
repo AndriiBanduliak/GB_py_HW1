20dd8215344aad13f9d980172c11ad5265a3648b
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 15:00:46 +0300

    1st task is done by function

--- hw1.py
+++ hw1.py
@@ -5,10 +5,25 @@
 - 1 -> нет'''
 
 
-day=int(input('Enter the number of the day: '))
-if day >0 and day <6:
-    print ('It is a work day :( ')
-elif day >5 and day <8:
-    print ("It's a weekend :) uhooo!!!")
-else:
-    print("You enter the wrong number!!!")
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("It's mot a number!")
+    return number
+
+
+def checkNumber(num):
+    if 6 <= num <= 7:
+        print("It's a weekend! :)")
+    elif 0 < num < 6:
+        print("It's a work day")
+    else:
+        print("Number outside 7 days")
+
+
+num = InputNumbers("Enter the number of the day: ")
+checkNumber(num)
