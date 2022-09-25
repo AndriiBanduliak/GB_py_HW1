6c04c1a2b4c054f640cfa4189a3c32a924b7cd0c
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 15:04:53 +0300

    2nd task is done by function

--- hw1.py
+++ hw1.py
@@ -2,9 +2,24 @@
 # утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для 
 # всех значений предикат.
 
-print("x y z")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not (x or y or z)==(not x and not y and not z):
-                print(x, y, z)
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Enter value{xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"The statement is true")
+else:
+    print(f"The statement is false")
