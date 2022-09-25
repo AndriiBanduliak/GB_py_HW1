0a1cfdcff1999f622b2361fa63dc8fe681471245
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-25 15:08:40 +0300

    3d task is done

--- hw1.py
+++ hw1.py
@@ -1,25 +1,24 @@
-#2. Напишите программу для. проверки истинности 
-# утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для 
-# всех значений предикат.
+'''3. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+ выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+*Пример:*
+- x=34; y=-30 -> 4
+- x=2; y=4-> 1
+- x=-34; y=-30 -> 3'''
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Enter value{xyz[i]}: "))
-    return a
-
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"The statement is true")
-else:
-    print(f"The statement is false")
+while True:
+    print('X ≠ 0 и Y ≠ 0 ')
+    x=int(input('Enter the number representing the coordinate X: '))
+    y=int(input('Enter the number representing the coordinate Y: '))
+    if not(y==0 or x==0):
+        break
+print('x=',x, 'y=',y, '-> ', end='')
+if (x>0):
+    if (y>0):
+        print('1')
+    else:
+        print('4')
+if (x<0):
+    if (y>0):
+        print('2')
+    else:
+        print('3')
